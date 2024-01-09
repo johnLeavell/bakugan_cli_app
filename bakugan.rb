d59848a066cc0c25499aeafba4e09dfa403b4a97
John Leavell <johnleavell@gmail.com>
2024-01-09 13:40:03 -0600

    displaying battle results

--- bakugan.rb
+++ bakugan.rb
@@ -1,8 +1,13 @@
 class Bakugan
-    attr_accessor :name
+    attr_accessor :name, :brawls_won
   
     def initialize(name)
       @name = name
+      @brawls_won = 0
+    end
+
+    def simulate_match(opponet)
+      rand(2).zero? ? self : opponet
     end
   end
   
--- battle.rb
+++ battle.rb
@@ -1,8 +1,10 @@
 require 'tty-prompt'
+
 class Battle
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @prompt = TTY::Prompt.new
   end
 
   def run
@@ -10,7 +12,7 @@ class Battle
     select_bakugans(@player1)
     select_bakugans(@player2)
     display_selected_bakugans
-    display_begin_battle
+    begin_battle
   end
 
   private
@@ -25,7 +27,8 @@ class Battle
     selected_bakugans = TTY::Prompt.new.multi_select("Choose Bakugans:", choices, min: 3, max: 3)
     
     selected_bakugans.each do |bakugan_name|
-      player.selected_bakugans << Bakugan.new(bakugan_name)
+      bakugan = Bakugan.new(bakugan_name)
+      player.selected_bakugans << bakugan
     end
   end
 
@@ -42,7 +45,50 @@ class Battle
     end
   end
 
-  def display_begin_battle
+  def begin_battle
     puts "Let the battle begin!"
+
+    player1_bakugan = select_bakugan(@player1)
+    player2_bakugan = select_bakugan(@player2)
+
+    battle_result = simulate_battle(player1_bakugan, player2_bakugan)
+
+    if battle_result
+      puts "#{@player1.name} wins the match!"
+      player1_bakugan.brawls_won += 1
+      @player1.wins += 1
+    else
+      puts "#{@player2.name} wins the match!"
+      player2_bakugan.brawls_won += 1
+      @player2.wins += 1
+    end
+
+    display_battle_results
+  end
+
+  def select_bakugan(player)
+    puts "#{player.name}, select your Bakugan for this match:"
+    display_player_bakugans(player)
+  
+    selected_bakugan_index = @prompt.select("Choose a Bakugan:", player.selected_bakugans.map(&:name))
+    player.selected_bakugans.find { |bakugan| bakugan.name == selected_bakugan_index }
+  end
+  
+
+  def simulate_battle(bakugan1, bakugan2)
+    bakugan1.simulate_match(bakugan2)
+  end
+
+  def display_battle_results
+    puts "\nBattle Results:"
+    display_player_results(@player1)
+    display_player_results(@player2)
+  end
+
+  def display_player_results(player)
+    puts "#{player.name} - Wins: #{player.wins}"
+    player.selected_bakugans.each do |bakugan|
+      puts "- #{bakugan.name} - Brawls Won: #{bakugan.brawls_won}"
+    end
   end
 end
